f2b21b17df6f2fecb95052256d3b73035a6030b5
xing <1515113603qq.com>
2021-01-28 17:28:02 +0800

    提交

--- src/main/java/com/xing/jenkinsdemo/controller/HelloController.java
+++ src/main/java/com/xing/jenkinsdemo/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @Description: TODO
+ * @Description: hello
  * @Author DXX
  * @Date 2021/1/28
  **/
